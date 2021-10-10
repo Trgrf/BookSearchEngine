370cc54c1a0a9121428427609e0c821c90be1790
Theo <trgrf1@gmail.com>
2021-10-10 12:47:16 -0400

    resolver for book

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,5 +3,9 @@ const { User, Book } = require('../models');
 const resolvers = {
     user: async () => {
         return User.find({});
-    }
+    },
+    book: async (parent, {_id}) => {
+        const params = _id ? {_id} : {};
+        return Book.find(params);
+    },
 },
