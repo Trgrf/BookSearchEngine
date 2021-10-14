06d8660c703383639953cde0833f488024572e8d
Theo <trgrf1@gmail.com>
2021-10-14 10:22:56 -0400

    refactoring savedBooks page

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,19 +1,17 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+import React from "react";
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 // import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
-import { useQuery } from '@apollo/client';
-import { useMutation } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
-
-  const [queryMe, {error}] = useQuery(QUERY_ME);
-
+  // const [userData, setUserData] = useState({});
+  const { loading, data} = useQuery(QUERY_ME);
   const [removeBook, {error}] = useMutation(REMOVE_BOOK);
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
